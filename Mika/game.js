e804fad3921e8054ab366dd80cd82a0bb16547e3
Mika <mpeersha@ucsc.edu>
2023-05-31 16:20:33 -0700

    improved cinematics

--- Mika/game.js
+++ Mika/game.js
@@ -6,20 +6,32 @@ class Introduction extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('orb', 'assets/Crackedorb.png');
+    this.load.image('leftorb', 'assets/Leftorb.png');
+    this.load.image('rightorb', 'assets/Rightorb.png');
+    this.load.image('forest', 'assets/forest_path.png');
+
   }
 
   create() {
     this.cameras.main.fadeIn(1000);
+    // background
+    this.bg = this.add.image(800,100, "forest").setScale(3.6).setDepth(-1);
+    this.leftBg = this.add.image(370,570, "forest").setScale(1.9).setAlpha(0).setDepth(-1);
+    this.rightBg = this.add.image(1220,570, "forest").setScale(1.9).setAlpha(0).setDepth(-1);
+
     // player holding an orb
     this.player = this.add.rectangle(400,400,150,300,0x000000);
-    this.orb = this.add.rectangle(500,400,100,100,0xff0000);
-    this.orb2 = this.add.rectangle(1200,400,100,50,0xff0000).setAlpha(0);
+    this.orb = this.add.image(500,400, 'orb').setScale(0.1); // cracked orb
+    this.orbLeft = this.add.image(1200,400, 'leftorb').setScale(0.1).setAlpha(0); // left side
+    this.orbRight = this.add.image(1070,400, 'rightorb').setScale(0.1).setAlpha(0); // right side
     this.hole = this.add.rectangle(1300,500,130,90,0x000000).setDepth(-1);
-    this.line = this.add.rectangle(800,600,20,1200, 0xffffff).setAlpha(0);
+    this.line = this.add.rectangle(800,600,20,1200, 0xffffff).setAlpha(0); // dividor
     this.playerPast = this.add.rectangle(200,400,150,300,0x000000).setAlpha(0);
-    this.orb2Past = this.add.rectangle(270,400,50,100,0xff0000).setAlpha(0);
+    this.orbLeftPast = this.add.image(270,400, 'leftorb').setScale(0.1).setAlpha(0);
     this.holePast = this.add.rectangle(500,500,130,90,0x000000).setDepth(-1).setAlpha(0);
-
+    
+    // Player and orb move from left side of the screen to the right side
     this.tweens.add({
       targets: [this.player],
       x: 1000,
@@ -34,17 +46,17 @@ class Introduction extends Phaser.Scene {
       delay: 1000,
       ease: 'Power2',
   });
+  // Make the cracked orb disappear, and show the two sides of it
   this.tweens.add({
     targets: [this.orb],
     x: 1100,
-    width: 50,
+    alpha: 0,
     duration: 2000,
     delay: 2500,
-    ease: 'Power2',
-      
+    ease: 'Power2',   
   });
   this.tweens.add({
-    targets: [this.orb2],
+    targets: [this.orbLeft],
     x: 1300,
     y: 500,
     alpha: 1,
@@ -52,26 +64,32 @@ class Introduction extends Phaser.Scene {
     delay: 2700,
     ease: 'Power2',
   });
-
   this.tweens.add({
-    targets: [this.orb2],
+    targets: [this.orbRight],
+    alpha: 1,
+    duration: 2000,
+    delay: 2700,
+    ease: 'Power2',
+  });
+  // In the split, change the background and add the left character
+  this.tweens.add({
+    targets: [this.orbLeft, this.bg],
     alpha: 0,
     duration: 2000,
     delay: 3400,
     ease: 'Power2',
   });
   this.tweens.add({
-    targets: [this.line, this.playerPast, this.orb2Past, this.holePast],
+    targets: [this.line, this.playerPast, this.orbLeftPast, this.holePast, this.leftBg, this.rightBg],
     alpha: 1,
     duration: 2000,
-    delay: 3900,
+    delay: 3500,
     ease: 'Power2',
     onComplete: () => {
       this.cameras.main.fadeOut(2000);
       this.scene.start("ending");
     }
   });
-
 }
  
   update() { }
@@ -86,29 +104,38 @@ class Ending extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('orb', 'assets/Crackedorb.png');
+    this.load.image('leftorb', 'assets/Leftorb.png');
+    this.load.image('rightorb', 'assets/Rightorb.png');
+    this.load.image('forest', 'assets/forest_path.png');
   }
 
   create() {
+    // fade in
     this.cameras.main.fadeIn(1000);
+    // forest background
+    this.bg = this.add.image(800,100, "forest").setScale(3.6).setAlpha(0).setDepth(-1);
+    this.leftBg = this.add.image(370,570, "forest").setScale(1.9).setDepth(-1);
+    this.rightBg = this.add.image(1220,570, "forest").setScale(1.9).setDepth(-1);
+
     // player holding an orb
     this.player = this.add.rectangle(1000,400,150,300,0x000000);
-    this.orb = this.add.rectangle(1070,400,50,100,0xff0000).setAlpha(1);
-    // this.hole = this.add.rectangle(1300,500,130,90,0x000000).setDepth(-1);
+    this.orb = this.add.image(1070,400, 'rightorb').setScale(0.1);
     this.line = this.add.rectangle(800,600,20,1200, 0xffffff).setAlpha(1).setDepth(-1);
     this.playerPast = this.add.rectangle(200,400,150,300,0x000000).setAlpha(1);
-    this.orbPast = this.add.rectangle(270,400,50,100,0xff0000).setAlpha(1);
-    // this.holePast = this.add.rectangle(500,500,130,90,0x000000).setDepth(-1).setAlpha(1);
+    this.orbPast = this.add.image(270,400, 'leftorb').setScale(0.1);
 
+    // Orbs and players from the past and present combining
     this.tweens.add({
       targets: [this.orb],
-      x: 820,
+      x: 825,
       duration: 2500,
       delay: 1000,
       ease: 'Power2',
     });
     this.tweens.add({
       targets: [this.orbPast],
-      x: 780,
+      x: 795,
       duration: 2500,
       delay: 1000,
       ease: 'Power2',
@@ -121,9 +148,20 @@ class Ending extends Phaser.Scene {
     ease: 'Power2', 
   }); 
 
+  // Change backgrounds
   this.tweens.add({
-    targets: [this.line],
+    targets: [this.line, this.leftBg, this.rightBg],
     alpha: 0,
+    duration: 2000,
+    delay: 3400,
+    ease: 'Power2',
+    onComplete: () => {
+      this.cameras.main.fadeOut(2000);
+    }
+  });
+  this.tweens.add({
+    targets: [this.bg],
+    alpha: 1,
     duration: 2000,
     delay: 3400,
     ease: 'Power2',
@@ -137,7 +175,7 @@ class Ending extends Phaser.Scene {
 }
 
 var config = {
-  scene: [Introduction, Ending],
+  scene: [Introduction,Ending],
   backgroundColor: 0x43D58C,
   scale: {
       mode: Phaser.Scale.FIT,
