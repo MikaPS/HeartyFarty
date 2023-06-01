064f6409bee1a4f769f7d02b1953e8824fd9d581
Mika <mpeersha@ucsc.edu>
2023-06-01 13:21:16 -0700

    added level progression

--- coreGameplay/game.js
+++ coreGameplay/game.js
@@ -9,6 +9,7 @@ class intro extends Phaser.Scene {
     this.y = 0;
     this.track = 0;
     this.currentSide = 1; // 0 present, 1 past
+    this.level = 1;
   }
 
   preload() {
@@ -26,95 +27,90 @@ class intro extends Phaser.Scene {
     this.cameras.main.setBackgroundColor('#000000');
     this.rightBg = this.add.image(1220,570, "forest").setScale(1.9).setDepth(-1).setAlpha(0.4);
 
-      
+    // Switch between past/present. Clicking on each side of the screen switches the current view
+    this.present = this.add.rectangle(395,600,800,1200,0x000000).setDepth(-2)
+      .setInteractive({useHandCursor: true})
+      .on('pointerdown', () => {
+          this.currentSide = 1;
+      });
+    this.past = this.add.rectangle(1200,600,800,1200,0x00000).setDepth(-2)
+      .setInteractive({useHandCursor: true})  
+      .on('pointerdown', () => {
+        this.currentSide = 0;
+      });
 
+    // General settings we will need for all levels
+    this.ball1 = this.physics.add.image(430, 330, 'ball');
+    this.ball2 = this.physics.add.image(850, 330, 'ball');
+    this.wall = this.physics.add.image(800, 600, 'wall');
+    this.wall.body.setImmovable(true);
+    this.wall.setScale(.1,10);
 
-  // Switch between past/present. Clicking on each side of the screen switches the current view
-  this.present = this.add.rectangle(395,600,800,1200,0x000000).setDepth(-2)
-    .setInteractive({useHandCursor: true})
-    .on('pointerdown', () => {
-        this.currentSide = 1;
+    this.physics.add.collider(this.ball1, this.wall, () => {
     });
-  this.past = this.add.rectangle(1200,600,800,1200,0x00000).setDepth(-2)
-    .setInteractive({useHandCursor: true})  
-    .on('pointerdown', () => {
-      this.currentSide = 0;
+    this.physics.add.collider(this.ball2, this.wall, () => {
     });
 
-  // Level 1 design
-  // Create a gate for both players
-  this.gate1 = this.physics.add.image(400, 600, 'wall');
-  this.gate1.body.setImmovable(true);
-  this.gate1.setScale(7.5,.1);
-  this.gate2 = this.physics.add.image(1200, 600, 'wall');
-  this.gate2.body.setImmovable(true);
-  this.gate2.setScale(7.5,.1);
-  // Button for both players, but in the past it's usable, and in the future it isn't
-  this.button1 = this.physics.add.image(150, 500, 'wall');
-  this.button1.body.setImmovable(true);
-  this.button1.setScale(.5,.1).setAlpha(0.6); // The idea is that over time, something covered the button in the present
-  this.button2 = this.physics.add.image(950, 500, 'wall');
-  this.button2.body.setImmovable(true);
-  this.button2.setScale(.5,.1); 
+    this.ball1.setScale(0.7);
+    this.ball2.setScale(0.7);
+    this.wall.setScale(.1,10);
+    this.ball1.setBounce(0);
+    this.ball2.setBounce(0);
+    this.wall.setBounce(0);
+    this.ball1.setCollideWorldBounds(true);
+    this.ball2.setCollideWorldBounds(true);
+    this.wall.setCollideWorldBounds(true);
 
-   // Set depth to 0
-  this.ball1 = this.physics.add.image(430, 330, 'ball');
-  this.ball2 = this.physics.add.image(850, 330, 'ball');
-  this.wall = this.physics.add.image(800, 600, 'wall');
-  this.wall.body.setImmovable(true);
-  this.wall.setScale(.1,10);
+    // Level 1 design
+    if (this.level == 1) {
+      // Create a gate for both players
+      this.gate1 = this.physics.add.image(400, 600, 'wall');
+      this.gate1.body.setImmovable(true);
+      this.gate1.setScale(7.5,.1);
+      this.gate2 = this.physics.add.image(1200, 600, 'wall');
+      this.gate2.body.setImmovable(true);
+      this.gate2.setScale(7.5,.1);
+      // Button for both players, but in the past it's usable, and in the future it isn't
+      this.button1 = this.physics.add.image(150, 500, 'wall');
+      this.button1.body.setImmovable(true);
+      this.button1.setScale(.5,.1).setAlpha(0.6); // The idea is that over time, something covered the button in the present
+      this.button2 = this.physics.add.image(950, 500, 'wall');
+      this.button2.body.setImmovable(true);
+      this.button2.setScale(.5,.1); 
 
-  this.physics.add.collider(this.ball1, this.wall, () => {
-  });
-  this.physics.add.collider(this.ball2, this.wall, () => {
-  });
-
-  // Gate collision
-  this.gate1Collision = this.physics.add.collider(this.ball1, this.gate1, () => {
-  });
-  this.gate2Collision = this.physics.add.collider(this.ball2, this.gate2, () => {
-  });
-  // Button collision
-  this.isButtonOn = false;
-  this.button1Collision = this.physics.add.collider(this.ball1, this.button1, () => {
-    
-  });
-  // Colliding with second button will remove the gates and buttons
-  this.button2Collision = this.physics.add.collider(this.ball2, this.button2, () => {
-    this.gate1.setAlpha(0);
-    this.gate2.setAlpha(0);
-    this.physics.world.removeCollider(this.gate1Collision);
-    this.physics.world.removeCollider(this.gate2Collision);
-    this.isButtonOn = true;
-  });
-
-  this.ball1.setScale(0.7);
-  this.ball2.setScale(0.7);
-  this.wall.setScale(.1,10);
-  this.ball1.setBounce(0);
-  this.ball2.setBounce(0);
-  this.wall.setBounce(0);
-  this.ball1.setCollideWorldBounds(true);
-  this.ball2.setCollideWorldBounds(true);
-  this.wall.setCollideWorldBounds(true);
-
-      // On screen controllers
-      this.pastRightKey = this.add.rectangle(1520,1120,75,75, 0xff0000);
-      this.pastDownKey = this.add.rectangle(1420,1120,75,75, 0xff0000)        
-      this.pastLeftKey = this.add.rectangle(1320,1120,75,75, 0xff0000);
-      this.pastUpKey = this.add.rectangle(1420,1020,75,75, 0xff0000);
-      this.presentRightKey = this.add.rectangle(1520,1120,75,75, 0xff0000);
-      this.presentDownKey = this.add.rectangle(1420,1120,75,75, 0xff0000)        
-      this.presentLeftKey = this.add.rectangle(1320,1120,75,75, 0xff0000);
-      this.presentUpKey = this.add.rectangle(1420,1020,75,75, 0xff0000);
+      // Gate collision
+      this.gate1Collision = this.physics.add.collider(this.ball1, this.gate1, () => {
+      });
+      this.gate2Collision = this.physics.add.collider(this.ball2, this.gate2, () => {
+      });
+      // Button collision
+      this.isButtonOn = false;
+      this.button1Collision = this.physics.add.collider(this.ball1, this.button1, () => {
+        
+      });
+      // Colliding with second button will remove the gates and buttons
+      this.button2Collision = this.physics.add.collider(this.ball2, this.button2, () => {
+        this.gate1.setAlpha(0);
+        this.gate2.setAlpha(0);
+        this.physics.world.removeCollider(this.gate1Collision);
+        this.physics.world.removeCollider(this.gate2Collision);
+        this.isButtonOn = true;
+      });
+    }
 
 
-      
-  // Add collision detection between ball and basket
-    let graphics = this.add.graphics();
-    graphics.lineStyle(4, 0xffffff, 1);
+    // On screen controllers
+    this.pastRightKey = this.add.rectangle(1520,1120,75,75, 0xff0000);
+    this.pastDownKey = this.add.rectangle(1420,1120,75,75, 0xff0000)        
+    this.pastLeftKey = this.add.rectangle(1320,1120,75,75, 0xff0000);
+    this.pastUpKey = this.add.rectangle(1420,1020,75,75, 0xff0000);
+    this.presentRightKey = this.add.rectangle(1520,1120,75,75, 0xff0000);
+    this.presentDownKey = this.add.rectangle(1420,1120,75,75, 0xff0000)        
+    this.presentLeftKey = this.add.rectangle(1320,1120,75,75, 0xff0000);
+    this.presentUpKey = this.add.rectangle(1420,1020,75,75, 0xff0000);
+
+
     // detect up and down arrow key presses
-
     const cursors = this.input.keyboard.createCursorKeys();
     this.input.keyboard.on('keydown', (event) => {
       // if current side is the past, can control both characters
@@ -198,6 +194,12 @@ class intro extends Phaser.Scene {
   }
 
   update() {
+    // Move to new levels
+    if (this.ball1.y >= 1150 && this.ball2.y >= 1150) {
+      if (this.level == 1) {
+        this.level = 2;
+      }
+    }
     // On screen controllers 
     if (this.currentSide == 0) {
       this.pastKeyboardMovement(this.pastRightKey, this.ball1, this.ball2,150,0);
@@ -213,11 +215,13 @@ class intro extends Phaser.Scene {
     }
 
     // checking collision with button to remove the objects upon collision
-    if (this.isButtonOn == true) {
-      this.button1.setAlpha(0);
-      this.button2.setAlpha(0);
-      this.physics.world.removeCollider(this.button1Collision);
-      this.physics.world.removeCollider(this.button2Collision);
+    if (this.level == 1) {
+      if (this.isButtonOn == true) {
+        this.button1.setAlpha(0);
+        this.button2.setAlpha(0);
+        this.physics.world.removeCollider(this.button1Collision);
+        this.physics.world.removeCollider(this.button2Collision);
+      }
     }
   }
 
